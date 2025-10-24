27e0d86428b9ff28c2d6974121ec66bd61c3cc3f
Luk-ESC <eschbacher.lukas@gmail.com>
2025-10-24 13:39:47 +0200

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,70 @@
+{
+  inputs = {
+    systems.url = "github:nix-systems/default";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      flake-utils,
+      naersk,
+      nixpkgs,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+        lib = pkgs.lib;
+
+        naersk' = pkgs.callPackage naersk { };
+      in
+      {
+        # For `nix build` & `nix run`:
+        defaultPackage = naersk'.buildPackage rec {
+          src = ./.;
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            makeWrapper # to be able to use wrapProgram
+          ];
+          buildInputs = with pkgs; [
+            libxkbcommon
+            libGL
+
+            # WINIT_UNIX_BACKEND=wayland
+            wayland
+
+            # WINIT_UNIX_BACKEND=x11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            xorg.libX11
+          ];
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+          postInstall = ''
+            wrapProgram $out/bin/megalodon --set LD_LIBRARY_PATH "${LD_LIBRARY_PATH}"
+          '';
+
+        };
+
+        # For `nix develop`:
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            rustc
+            cargo
+          ];
+        };
+      }
+    );
+}
